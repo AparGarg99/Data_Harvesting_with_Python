2c7379edb56c3494a7901c8682d213f566b374a4
Apar Garg <54896849+AparGarg99@users.noreply.github.com>
2023-01-15 13:10:27 +0800

    Update home.py

--- LifeSciences/apps/home.py
+++ LifeSciences/apps/home.py
@@ -5,4 +5,4 @@ from packages import *
 def app():
     for i in range(10):
         st.write('')
-    st.markdown("<h1 style='text-align: center; color:#e3edd1; font-size:900%; font-family:Century Gothic, cursive;'>BiosectRx</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: center; color:#e3edd1; font-size:700%; font-family:Century Gothic, cursive;'>LifeSciences</h1>", unsafe_allow_html=True)
